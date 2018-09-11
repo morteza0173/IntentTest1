4e5e11a85557f0da6fd71c03fd4941827047dab1
morteza0173 <morteza0173@outlook.com>
2018-09-11 15:26:31 +0430

    some change

--- app/src/main/java/com/intent/test/intenttest/ShowNumberActivity.java
+++ app/src/main/java/com/intent/test/intenttest/ShowNumberActivity.java
@@ -20,6 +20,7 @@ public class ShowNumberActivity extends AppCompatActivity {
         setContentView(R.layout.activity_show_number);
 
         final boolean[] txtAddMoneyBoolean = {true};
+        final boolean[] txtLoseMoneyBoolean = {true};
 
 
         View view_showNumberActivity = (View)findViewById(R.id.view_showNumberActivity_circelView);
@@ -29,13 +30,13 @@ public class ShowNumberActivity extends AppCompatActivity {
         Animation myAnimation1 = AnimationUtils.loadAnimation(G.context, R.anim.animation1);
         Animation myAnimation2 = AnimationUtils.loadAnimation(G.context, R.anim.animation2);
         final TextView txtAddMoney = (TextView)findViewById(R.id.textView_showNumberActivity_txtAddMoney);
-        TextView txtLoseMoney = (TextView) findViewById(R.id.textView_showNumberActivity_txtLoseMoney);
+        final TextView txtLoseMoney = (TextView) findViewById(R.id.textView_showNumberActivity_txtLoseMoney);
         final EditText edtAddMoney = (EditText)findViewById(R.id.edt_showNumberActivity_edtAdd);
-        EditText edtLoseMoney = (EditText)findViewById(R.id.edt_showNumberActivity_edtLose);
+        final EditText edtLoseMoney = (EditText)findViewById(R.id.edt_showNumberActivity_edtLose);
         final RadioButton radioAddMoneyFrist = (RadioButton)findViewById(R.id.radioButton_showNumberActivity_radiofiristAdd);
         final RadioButton radioAddMoneySecond = (RadioButton)findViewById(R.id.radioButton_showNumberActivity_radiosecondAdd);
-        RadioButton radioLoseMoneyFirist = (RadioButton)findViewById(R.id.radioButton_showNumberActivity_radiofiristLose);
-        RadioButton radioLoseMoneySecond = (RadioButton)findViewById(R.id.radioButton_showNumberActivity_radiosecondLose);
+        final RadioButton radioLoseMoneyFirist = (RadioButton)findViewById(R.id.radioButton_showNumberActivity_radiofiristLose);
+        final RadioButton radioLoseMoneySecond = (RadioButton)findViewById(R.id.radioButton_showNumberActivity_radiosecondLose);
 
         final TextView moneyNumber = (TextView)findViewById(R.id.textView_showNumberActivity_moneyNumber);
 
@@ -62,11 +63,14 @@ public class ShowNumberActivity extends AppCompatActivity {
         txtLoseMoney.setAnimation(myAnimation2);
         txtLoseMoney.setVisibility(View.VISIBLE);
 
+        final int[] moneyNumberInt = {Integer.valueOf(moneyNumber.getText().toString())};
+
+
         addMoney.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                int moneyNumberInt = Integer.valueOf(moneyNumber.getText().toString());
+
 
 
                 if(txtAddMoneyBoolean[0]){
@@ -82,10 +86,54 @@ public class ShowNumberActivity extends AppCompatActivity {
                     radioAddMoneySecond.setVisibility(View.GONE);
                     edtAddMoney.setVisibility(View.GONE);
                     txtAddMoneyBoolean[0] = true;
-                    moneyNumberInt += Integer.valueOf(edtAddMoney.getText().toString());
-                    moneyNumber.setText(moneyNumberInt);
+                    moneyNumberInt[0] += Integer.valueOf(edtAddMoney.getText().toString());
+
+                    int moneyNumberBeforChange = Integer.valueOf(moneyNumber.getText().toString());
+                    ValueAnimator numberAnimation1 = ValueAnimator.ofInt(moneyNumberBeforChange, moneyNumberInt[0]);
+                    numberAnimation1.setDuration(500);
+                    numberAnimation1.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                        @Override
+                        public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                            moneyNumber.setText(valueAnimator.getAnimatedValue().toString());
+                        }
+                    }); numberAnimation1.start();
 
                 }
+
+
+            }
+        });
+
+        loseMoney.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (txtLoseMoneyBoolean[0]) {
+                    txtLoseMoney.setText("ذخیره مخارج");
+                    radioLoseMoneyFirist.setVisibility(View.VISIBLE);
+                    radioLoseMoneySecond.setVisibility(View.VISIBLE);
+                    edtLoseMoney.setVisibility(View.VISIBLE);
+                    txtLoseMoneyBoolean[0] =false ;
+
+                }else {
+                    txtLoseMoney.setText("افزودن مخارج");
+                    radioLoseMoneyFirist.setVisibility(View.GONE);
+                    radioLoseMoneySecond.setVisibility(View.GONE);
+                    edtLoseMoney.setVisibility(View.GONE);
+                    txtLoseMoneyBoolean[0]=true;
+                    moneyNumberInt[0] -= Integer.valueOf(edtLoseMoney.getText().toString());
+                    int moneyNumberBeforChange1 = Integer.valueOf(moneyNumber.getText().toString());
+                    ValueAnimator numberAnimation2 = ValueAnimator.ofInt(moneyNumberBeforChange1, moneyNumberInt[0]);
+                    numberAnimation2.setDuration(500);
+                    numberAnimation2.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                        @Override
+                        public void onAnimationUpdate(ValueAnimator valueAnimator1) {
+                            moneyNumber.setText(valueAnimator1.getAnimatedValue().toString());
+                        }
+                    });numberAnimation2.start();
+
+                }
+
 
 
             }
